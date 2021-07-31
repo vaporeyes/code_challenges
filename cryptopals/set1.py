2660f4101507f2e1db73508afa26dba64d2a1b4d
pythonicqualms <pythonicqualms@gmail.com>
2021-07-31 08:01:11 -0500

    correct hex to b64 function

--- cryptopals/set1.py
+++ cryptopals/set1.py
@@ -1,10 +1,4 @@
-def hex2b64(h):
-    """
-    convert hex to base64
-    """
-    return h.decode("hex").encode("base64")
+from base64 import b64encode
 
-def xor_byte_strings(s1, s2):
-    s1 = bytes.fromhex(s1)
-    s2 = bytes.fromhex(s2)
-    return bytes([a ^ b for a,b in zip(s1, s2)]).hex()
+def hex2b64(s: str):
+    return b64encode(bytes.fromhex(s))
